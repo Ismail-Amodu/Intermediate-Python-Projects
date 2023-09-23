43487711b82635b11345bd0469198055039f7c4a
Ismail <amoduismail74@gmail.com>
2023-09-23 20:05:36 +0100

    This strengthen my knowledge on class and inheritance
    23-09-23

---
+++ scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+FONT = ("courier", 24, "normal")
+FONT_END = ("courier", 20, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 260)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=FONT_END)
